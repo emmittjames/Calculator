3c048918a0576e6c6c2349af2734e9108e4770cb
emmittjames <emmittjames1@gmail.com>
2022-12-19 22:33:16 -0500

    HTML buttons added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 function App() {
   return (
     <div className = "calculatorGrid">
-      <div className = "display">
-        
+      <div className = "topDisplay">
+        <div className = "previousOperation"></div>
+        <div className = "currentOperation"></div>
       </div>
+      <button>AC</button>
+      <button>±</button>
+      <button>%</button>
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button className = "spanTwo">0</button>
+      <button>.</button>
+      <button>=</button>
     </div>
   )
 }
